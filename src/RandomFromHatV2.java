f60326972986cedf69ec58915dea6e375d87cdf8
AntMenBreth <antmamam@gmail.com>
2018-10-09 13:38:07 -0400

    random hat v2!!

--- src/RandomFromHatV2.java
+++ src/RandomFromHatV2.java
@@ -1,47 +1,44 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
-public class RandomFromHat {
+public class RandomFromHatV2 {
     public static void main(String[] args){
-        int[] matchup=new int[8];
-        //Both for testing whether the numbers were used in the array
+        Scanner scan=new Scanner(System.in);
+        System.out.println("How many teams?");
+        int numbteams=scan.nextInt();
+        int[] matchup=new int[numbteams];
         int temp=0;
         int tempp=0;
-        //For telling the array where different numbers should be placed
         int x=0;
-        //For Calculating when the program should end
         int fin=0;
 
-        //While loop containing the main code
-        while (fin!=4){
-            temp=calc();
-            tempp=calc();
-            //Checks if Either Number Was Used prior
+
+        while (fin!=numbteams){
+            temp=calc(numbteams);
+            tempp=calc(numbteams);
             if (tf(matchup,temp)==false){
 
             }
             else if(tf(matchup,tempp)==false){
 
             }
-            //Checks whether the numbers are the same
             else if(temp==tempp){
 
             }
-            //Sets up the array for the next set of teams & prints the teams versing each other
             else{
                 matchup[x]=temp;
                 matchup[(x+1)]=tempp;
                 x=x+2;
                 System.out.println(temp+" vs "+tempp);
                 fin++;
+                System.out.println(Arrays.toString(matchup));
             }
         }
     }
-    //Simply Calculates a Number Between 1-8
-    public static int calc(){
-        int randomSize=(int)(Math.random()*8)+1;
+    public static int calc(int numbteams){
+        int randomSize=(int)(Math.random()*(numbteams-1)+1)+1;
         return randomSize;
     }
-    //Scans Array for numbers already given a place
     public static boolean tf(int[] array, int temp){
         int x=0;
         for (int i=0;i<array.length;i++){
