a6a9c8b1fba74bf3ec07b763005d12b395a5e6fa
AntMenBreth <antmamam@gmail.com>
2018-11-26 14:10:40 -0500

    Personality Test Project! Must Do!

--- src/AnthonyMendezPersonalityTest.java
+++ src/AnthonyMendezPersonalityTest.java
@@ -1,34 +1,73 @@
+import java.util.*;
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 public class AnthonyMendezPersonalityTest {
     public static void main(String[] args) throws FileNotFoundException{
+        int[] a=new int[70];
         Scanner input=new Scanner(new File("personality.txt"));
         double pickline=0;
         String s="";
-
-        while(input.hasNextLine()){
-            s=input.nextLine();
-            System.out.println(s);
-            if(s.length()!=70){
+        while(input.hasNextLine()) {
+            s = input.nextLine();
+            if(70<=s.length()){
+                for(int i=0;i<70;i++){
+                    a[i]=s.charAt(i);
+                }
+                printer(a);
             }
             else{
-                //printer(input.nextLine());
+                System.out.println(s+":");
             }
         }
-
-
     }
-    public static void printer(String answ){
+    public static void printer(int[] a){
         int count=0;
         int A=0;
         int B=0;
-        int dash=0;
-        while(count!=10){
-            if (answ.charAt(count) == "A") {
+        //How many introverts
+        int first=0;
+        //How many iNtuition
+        int second=0;
+        //How many feeling
+        int third=0;
+        //How many perceiving
+        int fourth=0;
+        while(count!=70){
+
+            if(a[count]==66){
+                B++;
+                if(count%7==0||count==1){
+                    first++;
+                }
+                else if(count==2||count==3||count==9||count==10||count==16||count==17||count==23||count==24||count==30||count==31||count==37||
+                     count==38||count==44||count==45||count==51||count==52||count==58||count==59||count==65||count==66);{
+                         second++;
+                }
+                 else if(count==4||count==5||count==11||count==12||count==18||count==19||count==25||count==26||count==32||count==33||count==39||
+                        count==40||count==46||count==47||count==53||count==54||count==60||count==61||count==67||count==68);{
+                    second++;
+                }
+                else{
+                    fourth++;
+                }
+
+                }
+
+            else{
             }
             count++;
         }
+        System.out.println(A+"A-"+B+"B ");
+        fourth*=10;
+        first*=10;
+        second*=10;
+        third*=10;
+        System.out.print("["+first+"%, "+second+"%, "+third+"%, "+fourth+"%]");
 
+
+
+        System.out.println();
     }
+
 }
